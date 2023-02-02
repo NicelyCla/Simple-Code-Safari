46fef497d72171ffd5e4decfbe8cd7ea3a49144e
Claudio Praticò <praticoclaudio@gmail.com>
2023-02-02 12:36:25 +0100

    Update Simple_Code_Safari_Python.py

--- Simple_Code_Safari_Python.py
+++ Simple_Code_Safari_Python.py
@@ -26,8 +26,21 @@ def parse_imports(file_path, parent_folder):
                     else:
                         if alias.name not in imports:
                             imports.append(alias.name)
-    return [os.path.join(parent_folder, i + ".py") for i in imports if i + ".py" in os.listdir(parent_folder)]
 
+    result = []
+    for i in imports:
+        for root, dirs, files in os.walk(parent_folder):
+            if i.split(".")[-1] + ".py" in files:
+                #return os.path.join(root, name)
+                result.append(os.path.join(root, i.split(".")[-1] + ".py"))
+    '''
+    result = []
+    for i in imports:
+        if i + ".py" in os.listdir(parent_folder):
+            result.append(os.path.join(parent_folder, i + ".py"))
+    return result
+    '''
+    return result
 
 
 def build_graph(folder_path):
@@ -42,7 +55,7 @@ def build_graph(folder_path):
 
             file_path = os.path.join(parent, filename)
             for imported_module in parse_imports(file_path, parent):
-                print(imported_module)
+
                 edge = pydot.Edge(imported_module, os.path.join(parent, filename), len=1.0, fillcolor="red")
                 #edge_with_label = pydot.Edge(imported_module, os.path.join(parent, filename), label=f"import {os.path.basename(imported_module)}", len=1.0, fillcolor="red")
 
